b5366e49d823d73d9e85989112c9fe4f407e9423
Valerie McCulloch <valeriemcculloch@wireless-10-105-85-163.umd.edu>
2017-06-25 17:55:21 -0400

    local list elements

--- Agenda/app/src/main/java/com/vmcculloch/agenda/TDActivity.java
+++ Agenda/app/src/main/java/com/vmcculloch/agenda/TDActivity.java
@@ -5,20 +5,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 
 
 public class TDActivity extends AppCompatActivity {
-    ScrollView container; // create a scrollView in which you can put all EditTexts
+    LinearLayout containerLayout;
     static int totalEditTexts = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_td);
+        containerLayout = (LinearLayout)findViewById(R.id.tdLayout);
 
 
 
@@ -35,8 +38,25 @@ public class TDActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Element Added", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                onPressed();
             }
         });
     }
+
+    public void onPressed() {
+        totalEditTexts++;
+        if (totalEditTexts > 100)
+            return;
+        EditText editText = new EditText(this);
+        containerLayout.addView(editText);
+        editText.setGravity(Gravity.RIGHT);
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) editText.getLayoutParams();
+        layoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
+        editText.setLayoutParams(layoutParams);
+        //if you want to identify the created editTexts, set a tag, like below
+        editText.setTag("EditText" + totalEditTexts);
+
+    }
+
 
 }
