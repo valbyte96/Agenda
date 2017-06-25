c57e340d07ae07073dc8b2db5765a7211d3e6d3d
Valerie McCulloch <valeriemcculloch@wireless-10-105-85-163.umd.edu>
2017-06-25 18:07:08 -0400

    save settings prefs

--- Agenda/app/src/main/java/com/vmcculloch/agenda/SettingsActivity.java
+++ Agenda/app/src/main/java/com/vmcculloch/agenda/SettingsActivity.java
@@ -1,13 +1,40 @@
 package com.vmcculloch.agenda;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+    private Switch rSwitch;
+    private Switch sSwitch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        rSwitch = (Switch) findViewById(R.id.reminderSwitch);
+        sSwitch = (Switch) findViewById(R.id.soundSwitch);
+
+        //save preferences
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        Boolean restoredReminder = prefs.getBoolean("ReminderOn", false);
+        Boolean restoredSound = prefs.getBoolean("soundOn",false);
+        if(restoredReminder!=false|restoredSound!=false){
+            rSwitch.setChecked(restoredReminder);
+            sSwitch.setChecked(restoredSound);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+        editor.putBoolean("ReminderOn", rSwitch.isChecked());
+        editor.putBoolean("soundOn",sSwitch.isChecked());
+        editor.commit();
+        super.onBackPressed();
     }
 }
